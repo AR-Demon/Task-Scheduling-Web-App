a868637074822a54e37870b0a68baf26b844ec79
prayashshakya20@gmail.com <prayashshakya20@gmail.com>
2023-04-16 15:57:37 +0545

    user stats schema complete

--- server/models/UserStats.js
+++ server/models/UserStats.js
@@ -5,11 +5,39 @@ const UserStatsSchema = new mongoose.Schema({
         type: String,
         required: true,
         min: 3,
-        max: 50,
+        unique:true,
+    },
+    userLevel:{
+        type: Number,
+        required:true,
+    },
+    userLevelExp:{
+        type: Number,
+        required: true,
+
+    },
+    userAttribute:{
+        strength:{
+            type: Number,
+            required : true, 
+        },
+        intelligence:{
+            type: Number,
+            required : true,
+        },
+        agility:{
+            type: Number,
+            required : true,
+        },
+        dexterity:{
+            type: Number,
+            required : true,
+        },
+        luck:{
+            type: Number,
+            required : true,
+        },
     }
-
-
-
-
-
-})
+},{timestamps: true});
+const UserStats = mongoose.model("UserStats",UserStatsSchema);
+export default UserStats;
