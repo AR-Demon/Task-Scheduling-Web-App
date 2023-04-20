894b3ef5ec9991f6d107ec1f3dd04582b7a2cd51
Ar-Demon <praffulthapa11@gmail.com>
2023-04-20 18:23:42 +0545

    Finish the creation of TODO schema

--- server/models/Todo.js
+++ server/models/Todo.js
@@ -1,7 +1,65 @@
 import mongoose from 'mongoose';
 
 const todoSchema = new mongoose.Schema({
-    userId:{},
+    userId:{
+        type: String,
+        required: true,
+    },
+    userName:{
+        type: String,
+        required: true,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique:true,
+    },
+    content:{
+        type: String,
+        required: true,
+    },
+    description:{
+        type: String,
+        required: true,
+    },
+    priority:{
+        type: Number,
+        required: true,
+    },
+    label:{
+        type:Array,
+        required:false,
+    },
+    attribute:{
+        type: String,
+        required: true,
+    },
+    checked:{
+        type: Boolean,
+        required: true,
+    },
+    completed_at:{
+        type: Date,
+        required: true,
+    },
+    due:{
+        _date:{
+            type: Timestamp,
+            required: true,
+        },
+        date:{
+            type: Date,
+            required: true,
+        },
+        isRecurring:{
+            type: Boolean,
+            required: true,
+        },
+        string:{
+            type: String,
+            required: true,
+        },
+    },
 });
 
 const Todo = mongoose.model("Todo", todoSchema);
