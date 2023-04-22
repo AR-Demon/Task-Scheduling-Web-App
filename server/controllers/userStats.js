1fe6507f3012111bff5efdd5703710c0bd53aba6
Ar-Demon <praffulthapa11@gmail.com>
2023-04-22 18:23:59 +0545

    finish create userstats and get userstats route and controlles

--- server/controllers/userStats.js
+++ server/controllers/userStats.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
 import UserStats from "../models/UserStats.js"
 
+/* create or set the stats to zero or base level */
 export const createStats = async(req,res) => {
     try {
-        const { Id } = req.query.Id;
+        const Id = req.query.Id;
         const newUserStats = new UserStats({
             user_Id:Id,
             userLevel:1,
@@ -28,21 +29,25 @@ export const createStats = async(req,res) => {
     }
 }
 
+/* update the stats of the user 
+    1.
+*/
 export const updateStats = async(req,res) => {
     try {
-        const { Id } = req.params;
-        const newUserStats = new UserStats({
-            userId:Id,
-        })
+        const user_Id = req.query.userId;
+
+        const userStats = await UserStats.findOne({user_Id});
+
     } catch (error) {
         res.status(500).json({error: error.message});
     }
 }
 
+/* to get stats of the user */
 export const getStats = async(req,res) => {
     try {
-        const { Id } = req.params;
-        const userStats = await UserStats.findOne({Id});
+        const userId= req.query.Id;
+        const userStats = await UserStats.findOne({user_Id:userId});
         res.status(200).json(userStats);
     } catch (error) {
         res.status(500).json({error: error.message});
