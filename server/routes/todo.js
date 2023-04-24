d9a7575e758fd3861f838583728c721774dd2cb8
Ar-Demon <praffulthapa11@gmail.com>
2023-04-24 19:03:14 +0545

    finish create todo and  get todo (single and multiple)

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -1,10 +1,12 @@
 import express from "express";
-import {createTodo} from "../controllers/todo.js";
+import {createTodo, GetTodos, GetTodo} from "../controllers/todo.js";
 
 const Routes = express.Router();
 
 /* TODO ROUTES */
 Routes.post("/todo", createTodo);
+Routes.get("/todo",GetTodo);
+Routes.get("/todos", GetTodos);
 
 
 export default Routes;
