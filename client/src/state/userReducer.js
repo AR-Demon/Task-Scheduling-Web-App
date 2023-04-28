784e0b773c1ff8e83b791cb6ca6845ae2274a27e
ArMonarch <praffulthapa12@gmail.com>
2023-04-28 19:47:34 +0545

    create userReducer

--- client/src/state/userReducer.js
+++ client/src/state/userReducer.js
@@ -6,3 +6,20 @@ const initialState = {
     token: null,
     todo: [],
 };
+const userSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers:{
+        setMode: (state) => {
+            state.mode = state.mode === "light" ? "dark" : "light"; 
+        },
+        setLogin: (state,action) => {
+            state.user = action.payload.user;
+            state.token = action.payload.token;
+        },
+        setLogout: (state) => {
+            state.user = null;
+            state.token = null;
+        }
+    },
+});
